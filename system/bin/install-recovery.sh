895d24bb2e764c0befaa32af660a572a12e19da5
Park Ju Hyung <qkrwngud825@gmail.com>
2015-09-09 19:48:33 +0900

    Merge N920CXXU1AOI1 #1
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- system/bin/install-recovery.sh
+++ system/bin/install-recovery.sh
@@ -1,6 +1,6 @@
 #!/system/bin/sh
-if ! applypatch -c EMMC:/dev/block/platform/15570000.ufs/by-name/RECOVERY:26908672:da7ebe620a9d3e16f74e44c5ad52328d00da8bad; then
-  applypatch EMMC:/dev/block/platform/15570000.ufs/by-name/BOOT:25999360:2e1f30a6f7cccef1e74c1bae118bc24cd61a9769 EMMC:/dev/block/platform/15570000.ufs/by-name/RECOVERY da7ebe620a9d3e16f74e44c5ad52328d00da8bad 26908672 2e1f30a6f7cccef1e74c1bae118bc24cd61a9769:/system/recovery-from-boot.p && log -t recovery "Installing new recovery image: succeeded" || log -t recovery "Installing new recovery image: failed"
+if ! applypatch -c EMMC:/dev/block/platform/15570000.ufs/by-name/RECOVERY:26908672:9b1042b770a3f0d2a0d7cee893ada39cf38adde0; then
+  applypatch EMMC:/dev/block/platform/15570000.ufs/by-name/BOOT:25999360:8e6fb3fca84438a5584b2b227b63322a20b3a1a1 EMMC:/dev/block/platform/15570000.ufs/by-name/RECOVERY 9b1042b770a3f0d2a0d7cee893ada39cf38adde0 26908672 8e6fb3fca84438a5584b2b227b63322a20b3a1a1:/system/recovery-from-boot.p && log -t recovery "Installing new recovery image: succeeded" || log -t recovery "Installing new recovery image: failed"
 else
   log -t recovery "Recovery image already installed"
 fi
